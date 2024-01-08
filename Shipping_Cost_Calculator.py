ac5835705a54a2d108a3c1137ae2ca15cddec045
lovingsvictor <119813164+lovingsvictor@users.noreply.github.com>
2024-01-07 20:54:45 -0600

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -9,3 +9,11 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
+
+ # Here is a new update by <your GitHub username>
+git config --global user.email "lovingsvictor@gmail.com"
+git config --global user.name "Victor Lovings"
+git add .
+git commit -m "added a new line to Shipping_Cost_Calculator.py"
+git status
+git push --set-upstream origin shipping_calculator_fixes
